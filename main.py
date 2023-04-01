fdebc040b536a53c3dfd049449495cf726973e6e
Febriansyah12 <54895743+Febriansyah12@users.noreply.github.com>
2023-04-01 10:13:57 +0700

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from utils import predict_text
 
-app = Flask(__name__)
+##app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
